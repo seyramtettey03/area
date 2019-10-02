0190efedf8d57baf9adfc0f591600a65392d1d74
Seyram <seyram.tettey.2022@student.lhschools.org>
2019-10-02 08:56:40 -0400

    preform calculations printed results

--- src/com/lickingheights/Main.java
+++ src/com/lickingheights/Main.java
@@ -17,11 +17,25 @@ public class Main {
         System.out.println("welcome to thr area caculater");
          System.out.println("do you want to find the are of a \"square\" or \"rectangle\"?");
         //todo build variables for equation
+       answer = lighthouse.nextLine();
+        if (answer.equals("square")){
+           System.out.println("what is the length of one side?");
+           base = lighthouse.nextInt();
+            area = base * base;
 
+        }
+
+        else{
+            System.out.println("what is the length of the base?" );
+            base = lighthouse.nextInt();
+            System.out.println("what is the length of the height?" );
+            height = lighthouse.nextInt();
+            area = base *height;
+        }
         //todo calculate
 
         // todo print results
-
+        System.out.println("The area of the " + answer + " is "+ area);
 
 
     }
