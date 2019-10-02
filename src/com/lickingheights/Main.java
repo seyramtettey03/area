0f1b562003a0ff04739bc6b6b9c86a934e458869
Seyram <seyram.tettey.2022@student.lhschools.org>
2019-10-02 08:38:08 -0400

    intailized scanner and wrote prompts

--- src/com/lickingheights/Main.java
+++ src/com/lickingheights/Main.java
@@ -1,20 +1,27 @@
 package com.lickingheights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// TODO DECLARE VARIABLES
-
+    int base;
+    int height;
+    int area;
+    String answer;
+    Scanner lighthouse;
         //TODO  initialize variables
-
+        lighthouse = new Scanner(System.in);
         //todo prompt the user
-
+        System.out.println("welcome to thr area caculater");
+         System.out.println("do you want to find the are of a \"square\" or \"rectangle\"?");
         //todo build variables for equation
 
         //todo calculate
 
         // todo print results
-        
+
 
 
     }
